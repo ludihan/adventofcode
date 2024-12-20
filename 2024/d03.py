b17e2850b043ce5854fc0eca5969a4c5f264e5c1
ludihan <65617704+ludihan@users.noreply.github.com>
2024-12-20 14:53:23 -0400

    2024 d03

--- 2024/d03.py
+++ 2024/d03.py
@@ -1,7 +1,48 @@
 import re
-with open('input.txt') as f:
+from typing import List
+
+
+def remove_disabled_instructions(input: List[str]) -> List[str]:
+    a = []
+    do = True
+    for x in input:
+        if x == "do()":
+            do = True
+            continue
+        if x == "don't()":
+            do = False
+            continue
+
+        if do and x != "do()" and x != "don't()":
+            a.append(x)
+
+    return a
+
+
+with open("input.txt") as f:
     input = f.read()
-    print('p01:', sum(
-        [int(x) * int(y) for x, y in
-         [re.findall(r'\d+', x) for x in
-          re.findall(r'mul\(\d+,\d+\)', input)]]))
+    print(
+        "p01:",
+        sum(
+            [
+                int(x) * int(y)
+                for x, y in [
+                    re.findall(r"\d+", x) for x in re.findall(r"mul\(\d+,\d+\)", input)
+                ]
+            ]
+        ),
+    )
+    print(
+        "p02:",
+        sum(
+            [
+                int(x) * int(y)
+                for x, y in [
+                    re.findall(r"\d+", x)
+                    for x in remove_disabled_instructions(
+                        re.findall(r"mul\(\d+,\d+\)|do\(\)|don't\(\)", input)
+                    )
+                ]
+            ]
+        ),
+    )
